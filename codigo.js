6dbc96a2b3f5f4b3215b5e010e2a900804cddbfb
ricdemgtz <157503078+ricdemgtz@users.noreply.github.com>
2024-02-27 18:51:17 -0600

    Add files via upload
    Implementacion de archivos para contruir el sitio web

---
+++ codigo.js
@@ -0,0 +1,44 @@
+function Automovil(marca, modelo, color, anio, titular) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.color = color;
+    this.anio = anio;
+    this.titular = titular;
+}
+
+let automoviles = [];
+
+function agregarCarro() {
+        let elementoMarca = document.getElementById("nMarca").value;
+        let elementoModelo = document.getElementById("nModelo").value;
+        let elementoColor = document.getElementById("nColor").value;
+        let elementoAnio = document.getElementById("nAnio").value;
+        let elementoTitular = document.getElementById("nTitular").value;
+
+        let nCarro = new Automovil(elementoMarca, elementoModelo,elementoColor, elementoAnio, elementoTitular)
+        automoviles.push(nCarro)
+        alert("El Automóvil ha sido agregado")
+}
+
+
+
+Automovil.prototype.venderAutomovil = function (nPropietario) {
+    this.titular = nPropietario;
+};
+
+Automovil.prototype.verAuto = function () {
+    return `${this.marca} ${this.modelo} ${this.color} - ${this.anio} - ${this.titular}`
+};
+
+Automovil.prototype.enceder = function () {
+    alert("El automovil esta en marcha")
+};
+
+function verRegistros() {
+    let lista = document.getElementById("registros")
+    for (let automovil of automoviles) {
+        let item = document.createElement("li")
+        item.innerText = automovil.verAuto();
+        lista.appendChild(item);
+    }
+}
